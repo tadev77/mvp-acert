ff48b551d67cf46d8043cb887186e94ada98f0a1
Tadeu Oliveira <tadeu.oliveira.e5@gmail.com>
2024-03-03 22:13:41 -0300

    fix: generates new file names

--- src/services/exportService.js
+++ src/services/exportService.js
@@ -1,12 +1,12 @@
 import PdfExportRepository from '../repositories/PdfExportRepository.js';
 import SvgReader from '../repositories/SvgReaderRepository.js';
 
-const fileName = crypto.randomUUID();
 const svgr = new SvgReader();
 const pdfer = new PdfExportRepository();
 
 function generatePDF (certificateData) {
   return new Promise((resolve, reject) => {
+		const fileName = crypto.randomUUID();
 		const certificatePath = `/tmp/certificates/${fileName}.pdf`;
 		const {width, height} = svgr.getFileDimensions(certificateData);
 		pdfer.exportPdf(height, width, certificatePath, certificateData).then(_ => {
